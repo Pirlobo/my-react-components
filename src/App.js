838318c1041c4eff3dcb0071db6931f03aecb6d9
Pirlobo <bonguyens2001@gmail.com>
2021-05-16 04:07:10 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import './App.css';
 import DropDown from './components/DropDown/DropDown';
 import Carousel from './components/Carousel/Carousel';
 function App() {
+
+  // Carousel's props
   let imageList = [ 
   'https://www.musicnotes.com/images2/promos/store/900x520_piano-min.jpg',
   'https://www.musicnotes.com/images2/promos/store/900x520_piano_newage-min.jpg', 
   'https://www.musicnotes.com/images2/promos/store/900x520_piano_country-min.jpg',
   'https://www.musicnotes.com/images2/promos/store/900x520_piano_christian-min.jpg',
 ]
+
+// DropDown's props
+let title = "Rao Vặt"
+let options = [
+  {href : "#", opt :  "Đăng việc làm"},
+  {href : "#", opt :  "Đăng bài bán nhà"},
+]
   return (
-    <Carousel imageList= {imageList}></Carousel>
-    // <DropDown></DropDown>
+    // <Carousel imageList= {imageList}></Carousel>
+    <DropDown title = {title} options = {options}></DropDown>
   );
 }
 
--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './dropDown.css'
-function DropDown() {
+function DropDown(props) {
+  let title = props.title
+  let options = props.options
+
     return (
       <div class="dropdown">
-      <button class="dropbtn">Rao Vặt 
+      <button class="dropbtn">{title} 
         <i class="fa fa-caret-down"></i>
       </button>
       <div class="dropdown-content">
-        <a href="#">Đăng việc làm</a>
-        <a href="#">Đăng bài bán nhà</a>
+      {options.map((option, index) => {
+          return <a key = {index} href={option.href}>{option.opt}</a>
+        })}
       </div>
     </div> 
     )
