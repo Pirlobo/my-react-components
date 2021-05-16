2639836e7721449ea7c4cb6b986433da99db7afb
Pirlobo <bonguyens2001@gmail.com>
2021-05-16 05:20:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import DropDown from './components/DropDown/DropDown';
-import Carousel from './components/Carousel/Carousel';
-import { imageList } from './props/CarouselProps';
-import { Title, Options } from './props/DropDownProps';
+import "./App.css";
+import DropDown from "./components/DropDown/DropDown";
+import Carousel from "./components/Carousel/Carousel";
+import { imageList } from "./props/CarouselProps";
+import { Title, Options } from "./props/DropDownProps";
 function App() {
-
   // Carousel's props
   let images = imageList;
 
-// DropDown's props
-let title = Title
-let options = Options;
+  // DropDown's props
+  let title = Title;
+  let options = Options;
+  
   return (
     // <Carousel imageList= {images}></Carousel>
-    <DropDown title = {title} options = {options}></DropDown>
+    <DropDown title={title} options={options}></DropDown>
   );
 }
 
--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import './dropDown.css'
-import PropTypes from 'prop-types';
+import React from "react";
+import "./dropDown.css";
+import PropTypes from "prop-types";
 const DropDown = (props) => {
   let defaultProps = {
-    title : props.title,
-    options : props.options
-  }
-    return (
-      <div className="dropdown">
-      <button className="dropbtn">{defaultProps.title} 
+    title: props.title,
+    options: props.options,
+  };
+  return (
+    <div className="dropdown">
+      <button className="dropbtn">
+        {defaultProps.title}
         <i className="fa fa-caret-down"></i>
       </button>
       <div className="dropdown-content">
-      {defaultProps.options.map((option, index) => {
-          return <a key = {index} href={option.href}>{option.opt}</a>
+        {defaultProps.options.map((option, index) => {
+          return (
+            <a key={index} href={option.href}>
+              {option.opt}
+            </a>
+          );
         })}
       </div>
-    </div> 
-    )
-}
+    </div>
+  );
+};
 DropDown.propTypes = {
   title: PropTypes.string,
-  options : PropTypes.array
+  options: PropTypes.array,
 };
 
-export default DropDown
+export default DropDown;
